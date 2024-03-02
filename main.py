f45ddde10e5e41ff8a47723ef28e7e238c923274
Laura M <lauramariaconesa@gmail.com>
2024-03-02 18:40:31 +0100

    problem with string values in vector embedding

--- main.py
+++ main.py
@@ -22,16 +22,38 @@ def hyperparameter_tuning(X_train, y_train, X_test, y_test):
     best_model.plot(X_test, y_test, "Best model decision boundary")
 
 
+def one_hot_encoding(train, test):
+    # get all unique strings from the train["emotions"] and test set
+    unique_emotions = set(train["emotion"].unique().tolist() + test["emotion"].unique().tolist())
+    # one hot encode the emotions
+    dict_one_hot = {}
+    num = 0
+    for emotion in unique_emotions:
+        dict_one_hot[emotion] = num
+        num += 1
+
+    train["emotion"] = train["emotion"].map(dict_one_hot)
+    test["emotion"] = test["emotion"].map(dict_one_hot)
+    # save the one hot encoding dictionary
+    pd.DataFrame.from_dict(data=dict_one_hot, orient='index').to_csv('data/one_hot_encoding.csv', header=False)
+    return train, test
+
+
 def main():
-    train = pd.read_csv("train_ready_for_WS_preprocessed.csv")
-    test = pd.read_csv("test_ready_for_WS_preprocessed.csv")
-    X = train["essay"]
-    y = train["emotion"]
-    print(X.head())
-    print(X.shape())
-    X_test = test["essay"]
-    y_test = test["emotion"]
-    hyperparameter_tuning(X, y, X_test, y_test)
+    train = pd.read_csv('data/train_ready_for_WS_vectorized_1-gram.csv', delimiter=';', header=0)
+    test = pd.read_csv('data/test_vectorized_1-gram.csv', delimiter=';', header=0)
+    print(train.columns)
+    print(test.columns)
+    train, test = one_hot_encoding(train, test)
+
+    train_X = train["vector_1-gram"]
+
+    train_y = train["emotion"]
+
+    test_X = test["vector_1-gram"]
+    test_y = test["emotion"]
+    print(train_X.head()[0].dtype)
+    hyperparameter_tuning(train_X, train_y, test_X, test_y)
 
 
 if __name__ == "__main__":
